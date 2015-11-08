1d746f3e1aa109b033d7f2ca6d21306f23186f3c
Bernardo Sulzbach <mafagafogigante@gmail.com>
2015-11-07 22:50:46 -0200

    Checking should ignore spaces and be permissive.
    Closes #15
    Closes #16

--- guesser/guesses.py
+++ guesser/guesses.py
@@ -1,10 +1,47 @@
+def levenshtein(a, b):
+    if len(a) < len(b):
+        return levenshtein(b, a)
+
+    # len(s1) >= len(s2)
+    if len(b) == 0:
+        return len(a)
+
+    previous_row = range(len(b) + 1)
+    for i, c1 in enumerate(a):
+        current_row = [i + 1]
+        for j, c2 in enumerate(b):
+            # j+1 instead of j since previous_row and current_row are one character longer
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1  # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
+
+
+def normalize_and_check_for_fuzzy_equality(a, b):
+    """
+    Compares two strings case-insensitively for fuzzy equality according to the rules specified in the --help output.
+    """
+    words_of_a = [word for word in a.lower().split()]
+    words_of_b = [word for word in b.lower().split()]
+    if len(words_of_a) != len(words_of_b):
+        return False
+    # len(words_of_a) == len(words_of_b) is true here
+    pairs = [(words_of_a[i], words_of_b[i]) for i in range(len(words_of_a))]
+    distances = [levenshtein(*pair) for pair in pairs]
+    # We allow for floor(word_count / 2) typos, with a maximum of one typo (levenshtein distance of one) per word.
+    return sum(distances) <= int(len(words_of_a) / 2) and all(distance <= 1 for distance in distances)
+
+
 class Guess(object):
     def __init__(self, artist, title):
         self.artist = artist
         self.title = title
 
     def check_artist(self, song):
-        return self.artist.lower() == song.artist.lower()
+        return normalize_and_check_for_fuzzy_equality(self.artist, song.artist)
 
     def check_title(self, song):
-        return self.title.lower() == song.title.lower()
+        return normalize_and_check_for_fuzzy_equality(self.title, song.title)
--- tests/test_guess.py
+++ tests/test_guess.py
@@ -5,6 +5,29 @@ import guesser.guesses
 
 
 class TestGuess(TestCase):
+    def test_check_artist_base_case(self):
+        song = guesser.songs.Song("right", "title", ["A", "B"])
+        right_guess = guesser.guesses.Guess("right", "title")
+        self.assertTrue(right_guess.check_artist(song))
+        wrong_guess = guesser.guesses.Guess("wrong", "title")
+        self.assertFalse(wrong_guess.check_artist(song))
+
+    def test_check_artist_should_ignore_case(self):
+        song = guesser.songs.Song("Right", "title", ["A", "B"])
+        right_guess = guesser.guesses.Guess("right", "title")
+        self.assertTrue(right_guess.check_artist(song))
+        wrong_guess = guesser.guesses.Guess("Wrong", "title")
+        self.assertFalse(wrong_guess.check_artist(song))
+
+    def test_check_artist_should_ignore_whitespace(self):
+        song = guesser.songs.Song("right true", "title", ["A", "B"])
+        right_guess = guesser.guesses.Guess("right true", "title")
+        self.assertTrue(right_guess.check_artist(song))
+        right_guess = guesser.guesses.Guess("right  true", "title")
+        self.assertTrue(right_guess.check_artist(song))
+        right_guess = guesser.guesses.Guess("right   true", "title")
+        self.assertTrue(right_guess.check_artist(song))
+
     def test_check_title_base_case(self):
         song = guesser.songs.Song("artist", "right", ["A", "B"])
         right_guess = guesser.guesses.Guess("artist", "right")
@@ -19,16 +42,48 @@ class TestGuess(TestCase):
         wrong_guess = guesser.guesses.Guess("artist", "Wrong")
         self.assertFalse(wrong_guess.check_title(song))
 
-    def test_check_artist_base_case(self):
-        song = guesser.songs.Song("right", "title", ["A", "B"])
-        right_guess = guesser.guesses.Guess("right", "title")
-        self.assertTrue(right_guess.check_artist(song))
-        wrong_guess = guesser.guesses.Guess("wrong", "title")
-        self.assertFalse(wrong_guess.check_artist(song))
+    def test_check_title_should_ignore_whitespace(self):
+        song = guesser.songs.Song("artist", "right true", ["A", "B"])
+        right_guess = guesser.guesses.Guess("artist", "right true")
+        self.assertTrue(right_guess.check_title(song))
+        right_guess = guesser.guesses.Guess("artist", "right  true")
+        self.assertTrue(right_guess.check_title(song))
+        right_guess = guesser.guesses.Guess("artist", "right   true")
+        self.assertTrue(right_guess.check_title(song))
 
-    def test_check_artist_should_ignore_case(self):
-        song = guesser.songs.Song("Right", "title", ["A", "B"])
-        right_guess = guesser.guesses.Guess("right", "title")
-        self.assertTrue(right_guess.check_artist(song))
-        wrong_guess = guesser.guesses.Guess("Wrong", "title")
-        self.assertFalse(wrong_guess.check_artist(song))
+    def test_checks_should_allow_for_small_typos_in_artist(self):
+        # It is also asserted that the artist guess check does not influence the title guess check.
+        song = guesser.songs.Song("Iron Maiden", "These Colours Don't Run", ["A", "B"])
+        perfect_guess = guesser.guesses.Guess("Iron Maiden", "These Colours Don't Run")
+        self.assertTrue(perfect_guess.check_artist(song))
+        self.assertTrue(perfect_guess.check_title(song))
+        artist_one_character_typo = guesser.guesses.Guess("Iron Maden", "These Colours Don't Run")
+        self.assertTrue(artist_one_character_typo.check_artist(song))
+        self.assertTrue(artist_one_character_typo.check_title(song))
+        # Too many mistakes, the following artist guesses should fail.
+        artist_too_many_typos = guesser.guesses.Guess("Iron Made", "These Colours Don't Run")
+        self.assertFalse(artist_too_many_typos.check_artist(song))
+        self.assertTrue(artist_too_many_typos.check_title(song))
+        artist_even_more_typos = guesser.guesses.Guess("Air Made", "These Colours Don't Run")
+        self.assertFalse(artist_even_more_typos.check_artist(song))
+        self.assertTrue(artist_even_more_typos.check_title(song))
+
+    def test_checks_should_allow_for_small_typos_in_title(self):
+        # It is also asserted that the title guess check does not influence the artist guess check.
+        song = guesser.songs.Song("Iron Maiden", "These Colours Don't Run", ["A", "B"])
+        perfect_guess = guesser.guesses.Guess("Iron Maiden", "These Colours Don't Run")
+        self.assertTrue(perfect_guess.check_artist(song))
+        self.assertTrue(perfect_guess.check_title(song))
+        title_one_character_typo = guesser.guesses.Guess("Iron Maiden", "These Colors Don't Run")
+        self.assertTrue(title_one_character_typo.check_artist(song))
+        self.assertTrue(title_one_character_typo.check_title(song))
+        title_two_characters_typo = guesser.guesses.Guess("Iron Maiden", "These Colors Dont Run")
+        self.assertTrue(title_two_characters_typo.check_artist(song))
+        self.assertTrue(title_two_characters_typo.check_title(song))
+        # Too many mistakes, the following title guesses should fail.
+        title_too_many_typos = guesser.guesses.Guess("Iron Maiden", "This Colors Do Run")
+        self.assertTrue(title_too_many_typos.check_artist(song))
+        self.assertFalse(title_too_many_typos.check_title(song))
+        title_even_more_typos = guesser.guesses.Guess("Iron Maiden", "Deez Colors Do Run")
+        self.assertTrue(title_even_more_typos.check_artist(song))
+        self.assertFalse(title_even_more_typos.check_title(song))
